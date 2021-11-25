885ba6e2978aa74de44524b29b531c216fd4b344
tran0460 <tran0460@algonquinlive.com>
2021-11-25 18:10:40 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//app is for general control over the application
+//and connections between the other components
+const APP = {
+  init: () => {
+    //this function runs when the page loads
+  },
+};
+
+//search is for anything to do with the fetch api
+const SEARCH = {};
+
+//actors is for changes connected to content in the actors section
+const ACTORS = {};
+
+//media is for changes connected to content in the media section
+const MEDIA = {};
+
+//storage is for working with localstorage
+const STORAGE = {
+  //this will be used in Assign 4
+};
+
+//nav is for anything connected to the history api and location
+const NAV = {
+  //this will be used in Assign 4
+};
+
+//Start everything running
+
+APP.init();
